931040b4ed282dde5e9ce3ed14509c85903c6a02
CarlosJRF <99054641+CarlosJRF@users.noreply.github.com>
2026-03-30 16:58:25 -0300

    Iniciando repositorio con estructura base

---
+++ unidosPorLosAnimales/src/main/java/com/veterinaria/unidosPorLosAnimales/UnidosPorLosAnimalesApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.unidosPorLosAnimales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidosPorLosAnimalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidosPorLosAnimalesApplication.class, args);
+	}
+
+}
---
+++ unidosPorLosAnimales/src/test/java/com/veterinaria/unidosPorLosAnimales/UnidosPorLosAnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.unidosPorLosAnimales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidosPorLosAnimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
